ee6a49d1d92d6068b3b4a6dc8dbe5accf5cfd17b
metemer001@gmail.com <metemer001@gmail.com>
2022-07-10 21:29:48 +0200

    bring in the registering code from CloudRig

--- __init__.py
+++ __init__.py
@@ -31,7 +31,9 @@ bl_info = {
 	"tracker_url": "https://github.com/Mets3D/mets_tools/issues/new",
 }
 	
+from typing import List
 import importlib
+from bpy.utils import register_class, unregister_class
 
 from . import create_lightmap_uvs
 from . import make_physics_bones
@@ -80,14 +82,35 @@ modules = [
 	better_bone_extrude
 ]
 
-def register():
+def register_unregister_modules(modules: List, register: bool):
+	"""Recursively register or unregister modules by looking for either
+	un/register() functions or lists named `registry` which should be a list of 
+	registerable classes.
+	"""
+	register_func = register_class if register else unregister_class
+
 	for m in modules:
-		importlib.reload(m)
-		try:
+		if register:
+			importlib.reload(m)
+		if hasattr(m, 'registry'):
+			for c in m.registry:
+				try:
+					register_func(c)
+				except Exception as e:
+					un = 'un' if not register else ''
+					print(f"Warning: CloudRig failed to {un}register class: {c.__name__}")
+					print(e)
+
+		if hasattr(m, 'modules'):
+			register_unregister_modules(m.modules, register)
+
+		if register and hasattr(m, 'register'):
 			m.register()
-		except:
-			pass
+		elif hasattr(m, 'unregister'):
+			m.unregister()
+
+def register():
+	register_unregister_modules(modules, True)
 
 def unregister():
-	for m in modules:
-		m.unregister()
+	register_unregister_modules(modules, False)
